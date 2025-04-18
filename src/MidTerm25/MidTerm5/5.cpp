3a09544f0d21678d228d4aceb15d907609fd2886
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-04-18 14:06:36 +0900

    Create 5.cpp

---
+++ src/MidTerm25/MidTerm5/5.cpp
@@ -0,0 +1,77 @@
+#include "5.h"
+
+// 리스트 구조체 초기화
+void List_Init(List* list) {
+    list->arr = NULL;
+    list->listMaxSize = 0;
+    list->size = 0;
+    list->created = 0;
+}
+
+// 리스트 생성
+void List_Create(List* list, int maxSize) {
+    if (list->created) {
+        printf("리스트가 이미 생성되어 있습니다.\n");
+        return;
+    }
+    list->arr = (int*)malloc(sizeof(int) * maxSize);
+    list->listMaxSize = maxSize;
+    list->size = 0;
+    list->created = 1;
+    printf("리스트가 생성되었습니다.\n");
+}
+
+// 요소 삽입
+void List_Insert(List* list, int pos, int item) {
+    if (!list->created) {
+        printf("리스트가 존재하지 않습니다.\n");
+        return;
+    }
+    if (List_IsFull(list)) {
+        printf("리스트가 가득 찼습니다.\n");
+        return;
+    }
+    if (pos < 0 || pos > list->size) {
+        printf("연속되지 않은 위치에 삽입할 수 없습니다.\n");
+        return;
+    }
+    for (int i = list->size; i > pos; i--)
+        list->arr[i] = list->arr[i - 1];
+    list->arr[pos] = item;
+    list->size++;
+}
+
+// 리스트 비어있는지 여부
+int List_IsEmpty(List* list) {
+    return list->size == 0;
+}
+
+// 리스트 가득 차 있는지 여부
+int List_IsFull(List* list) {
+    return list->size == list->listMaxSize;
+}
+
+// 맨 끝에 요소 추가
+void List_Append(List* list, int item) {
+    if (!list->created) {
+        printf("리스트가 존재하지 않습니다.\n");
+        return;
+    }
+    if (List_IsFull(list)) {
+        printf("리스트가 가득 찼습니다.\n");
+        return;
+    }
+    list->arr[list->size++] = item;
+}
+
+// 리스트 출력
+void List_Print(List* list) {
+    if (!list->created) {
+        printf("오류: 리스트가 존재하지 않습니다.\n");
+        return;
+    }
+    printf("리스트 내용: ");
+    for (int i = 0; i < list->size; i++)
+        printf("%d ", list->arr[i]);
+    printf("\n");
+}
