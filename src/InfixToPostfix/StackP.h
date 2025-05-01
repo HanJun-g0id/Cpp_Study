1a7392e073f2afe4570e679a9a5e3f947fab87e0
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-01 21:44:35 +0900

    Create StackP.h

---
+++ src/InfixToPostfix/StackP.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct node {
+    int Item;
+    struct node* Next;
+} node;
+
+typedef node* Nptr;
+
+void Init(Nptr* Top);
+bool IsEmpty(Nptr Top);
+void Push(Nptr* Top, int Item);
+int Pop(Nptr* Top);
+void FreeList(Nptr* Top);
+char Peek(Nptr Top);
+void PrintStack(Nptr Top);
+
+void InfixToPostfix(const char* expr);
