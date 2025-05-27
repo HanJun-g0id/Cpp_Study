3e1f8258ed9121c8c827fbcb7c648bf2792877a8
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-27 11:24:47 +0900

    Create binaryTree_t.c

---
+++ src/BinaryTree/binaryTree_t.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "binaryTree.h"
+
+int main() {
+    treeType tree;
+    initTree(tree);
+
+    int park = createNode(tree, "Park");
+    int kim = createNode(tree, "Kim");
+    int seo = createNode(tree, "Seo");
+    int lee = createNode(tree, "Lee");
+    int cho = createNode(tree, "Cho");
+
+    setLeftChild(tree, park, kim);
+    setRightChild(tree, park, seo);
+    setLeftChild(tree, kim, lee);
+    setRightChild(tree, seo, cho);
+
+    printf("중위 순회 결과: ");
+    inorderTraversal(tree, park);
+    printf("\n");
+
+    return 0;
+}
