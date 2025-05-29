d30a2be3bd0f40b8438c297556a7daa33eee9fbf
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-29 10:38:40 +0900

    Create BST.h

---
+++ src/BST/BST.h
@@ -0,0 +1,16 @@
+typedef struct node {
+    int Data;
+    char* Name;
+    struct node* LChild, *RChild;
+} NODE, *Nptr;
+
+void Visit(char* name);
+
+Nptr Create(int S[]);
+void Delete(Nptr &T, int Key);
+Nptr Insert(Nptr T, int key, const char* name);
+Nptr Search(Nptr T, int key);
+
+void PreOrder(Nptr T);
+void InOrder(Nptr T);
+void PostOrder(Nptr T);
