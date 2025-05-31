3fc2d0592ce33b1a6fc16046d1d277f5d2e9f357
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-05-31 19:59:51 +0900

    Create HeapSort_t.cpp

---
+++ src/HeapSort/HeapSort_t.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "HeapSort.h"
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("정렬 전 배열:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    heap_sort(arr, n);
+
+    printf("정렬 후 배열:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
