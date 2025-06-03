6fa0235486274384cbb97d0984d2f03903202f3c
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-06-03 14:24:40 +0900

    Create ReverseStack.h

---
+++ src/ReverseStack/ReverseStack.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct node {
+    int Item;
+    struct node* Next;
+} node;
+
+typedef node* Nptr;
+
+void Init(Nptr* Top);
+bool IsEmpty(Nptr Top);
+void Push(Nptr* Top, int Item);
+int Pop(Nptr* Top);
+void FreeList(Nptr* Top);
+char Peek(Nptr Top);
+void PrintStack(Nptr Top);
+
+void Reverse(char* str);
